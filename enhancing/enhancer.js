b23bff69a9d5657d20322ade6a6dc1b01462a11a
unknown <abdinajibmohamud@gmail.com>
2020-10-15 07:49:08 -0700

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,62 +4,29 @@ module.exports = {
   repair,
   get,
 };
-const object = {
-  name: 'fire',
-  durability:100,
-  enhancement:0
-}
 
 function success(item) {
-  if(item.enhancement===20){
-    return {...item, enhancement}
-  } else
-  return { ...item, enhancement:enhancement+1 };
+  if(item.enhancement <= 19){
+    return {...item, enhancement: item.enhancement + 1}
+  } else {
+    return {...item}
+  }
 }
 
 function fail(item) {
-  if(item.enhancement<15){
-    return { ...item, durability:durability-5 };
+  if (item.enhancement < 15){
+    return {...item, durability: item.durability - 5}
+  } else if (item.enhancement = 15) {
+    return {...item, durability: item.durability - 10}
+  } else {
+    return {...item, enhancement: item.enhancement - 1}
   }
-  else if(item.enhancement===15){
-    return { ...item, durability:durability-10 };
-  }
-  else if(item.enhancement>20){
-    return { ...item, durability:durability-1 };
-  }
-  
 }
 
 function repair(item) {
-  return { ...item, durability:100 };
+  return { ...item,  durability: 100};
 }
 
 function get(item) {
   return { ...item };
 }
-
-
-// - a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
-// - a `success(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement success.
-// - a `fail(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement failure.
-// - a `get()` method for use when working on the stretch problem.
-
-// The following sections list information provided by the client about `items` and the game's enhancing system.
-
-// ### Items.
-
-// - Items have `name`, `durability` and `enhancement`.
-// - The item's `enhancement` it's a number from 0 to 20.
-// - The item's `durability` it's a number from 0 to 100.
-
-// ### When enhancement succeeds
-
-// x The item's enhancement increases by 1.
-// x If the item enhancement level is 20, the enhancement level is not changed.
-// x The durability of the item is not changed.
-
-// ### When enhancement fails
-
-// x If the item's enhancement is less than 15, the durability of the item is decreased by 5.
-// x If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
-// x If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+it("adds to enhancement under 20", function () {
+    const item = {
+        name: "Bill",
+        durability: 30,
+        enhancement: 13
+    }
+    expect(enhancer.success(item)).toStrictEqual({
+        name: "Bill",
+        durability: 30,
+        enhancement: 14
+    })
+})
+it("increments durability based on enhancement", function () {
+    const item = {
+        name: "Bill",
+        durability: 30,
+        enhancement: 16
+    }
+    expect(enhancer.fail(item)).toStrictEqual({
+        name: "Bill",
+        durability: 20,
+        enhancement: 15
+    })
+})
+it("repairs the item", function () {
+    const item = {
+        name: "Bill",
+        durability: 10,
+        enhancement: 16
+    }
+    expect(enhancer.repair(item)).toStrictEqual({
+        name: "Bill",
+        durability: 100,
+        enhancement: 16
+    })
+})
